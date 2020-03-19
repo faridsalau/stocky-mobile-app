7872b4166bcc15840ad840a941b2691128a45e67
Farid Salau <max.zip848@gmail.com>
2020-03-19 16:17:15 -0500

    Add login and profile pages

--- MyApp/src/app/app-routing.module.ts
+++ MyApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () => import("./tabs/tabs.module").then(m => m.TabsPageModule)
+  },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./public/login/login.module").then(m => m.LoginPageModule)
   }
 ];
 @NgModule({
