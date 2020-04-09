66975079c25c2f0badb90d65ba768a5afb7c9fd1
Farid Salau <max.zip848@gmail.com>
2020-04-09 16:53:20 -0500

    Create add shoe to firebase function

--- MyApp/src/app/tab4/tab4.page.ts
+++ MyApp/src/app/tab4/tab4.page.ts
@@ -1,30 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController } from "@ionic/angular";
+import { Component, OnInit } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Router } from "@angular/router";
 
+interface Shoe {
+  name?: string;
+  url?: string;
+  condition?: string;
+}
+
 @Component({
-  selector: 'app-tab4',
-  templateUrl: './tab4.page.html',
-  styleUrls: ['./tab4.page.scss'],
+  selector: "app-tab4",
+  templateUrl: "./tab4.page.html",
+  styleUrls: ["./tab4.page.scss"],
 })
 export class Tab4Page implements OnInit {
+  shoe: Shoe = {
+    name: "",
+    url: "",
+    condition: "",
+  };
 
   constructor(
-    private alertController: AlertController,
     public afAuth: AngularFireAuth,
     private fireStore: AngularFirestore,
     private router: Router
+  ) {}
 
-  ) { }
-
-    onFileSelected(event){
-      console.log(event);
-    }
-
-
-  ngOnInit() {
+  async submit() {
+    let username;
+    this.afAuth.auth.onAuthStateChanged((user) => {
+      username = user.displayName;
+    });
+    const db = this.fireStore.firestore;
+    const ref = await db.collection("user_shoes");
+    let newShoe = {
+      createdAt: new Date().toISOString(),
+      hasOffer: false,
+      shoeName: this.shoe.name,
+      shoeUrl: this.shoe.url,
+      condition: this.shoe.condition,
+      username,
+    };
+    ref
+      .add(newShoe)
+      .then(() => {
+        alert(`${this.shoe.name} shoe has been added`);
+        this.shoe = {
+          name: "",
+          url: "",
+          condition: "",
+        };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
+  ngOnInit() {}
 }
