c105da5ee9f8d6e4cc0a26e7c9d9df5fd413f6e7
Farid Salau <max.zip848@gmail.com>
2020-04-09 15:50:11 -0500

    Fix errors

--- MyApp/src/app/tab4/tab4.module.ts
+++ MyApp/src/app/tab4/tab4.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { IonicModule } from "@ionic/angular";
+import { ExploreContainerComponentModule } from "../explore-container/explore-container.module";
 // import { Tab4PageRoutingModule } from './tab4-routing.module';
 
-import { Tab4Page } from './tab4.page';
+import { Tab4Page } from "./tab4.page";
 
 @NgModule({
   imports: [
@@ -14,7 +14,9 @@ import { Tab4Page } from './tab4.page';
     FormsModule,
     IonicModule,
     // Tab4PageRoutingModule
+    RouterModule.forChild([{ path: "", component: Tab4Page }]),
+    ExploreContainerComponentModule,
   ],
-  declarations: [Tab4Page]
+  declarations: [Tab4Page],
 })
 export class Tab4PageModule {}
